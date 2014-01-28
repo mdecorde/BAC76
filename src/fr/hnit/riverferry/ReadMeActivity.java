2d7e9ed0a33b90b696c083c91bc49d9f2ef9890a
mdecorde <matthieu.decorde@ens-lyon.fr>
2014-01-28 19:36:19 +0100

    v1

---
+++ src/fr/hnit/riverferry/ReadMeActivity.java
@@ -0,0 +1,46 @@
+//This file is part of RiverFerry.
+//
+//RiverFerry is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//RiverFerry is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with RiverFerry.  If not, see <http://www.gnu.org/licenses/>.
+//
+//Author: Matthieu Decorde 
+//Contact: I won't spread my email address, Please create an issue "contact" on Github with comment "@mdecorde ping"
+
+package fr.hnit.riverferry;
+
+import fr.hnit.riverferry.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+/**
+ * Readme activity
+ * 
+ * @author mdecorde
+ *
+ */
+public class ReadMeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_read_me);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+}
